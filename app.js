6d32b9721cb353b2d6b15f9aa138b96e65ab8082
Oshane Design <40554384+oshanedesign@users.noreply.github.com>
2022-01-14 15:53:30 -0500

    Add files via upload
    another upgrade

--- app.js
+++ app.js
@@ -6,15 +6,25 @@ const pokemonButton = document.querySelector('#pokemonButton')
 const cardImage = document.querySelector('#cardImage')
 const baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
-
+//let set = false;
 
 
 pokemonButton.addEventListener ('click', function() {
-    for (let i= 1; i < 152; i++){
-        const newImage = document.createElement('img');
-    newImage.src = `${baseUrl}${i}.png`
+    const newImage = document.getElementById('pokeImage');
+    newImage.src = `${baseUrl}${(Math.floor(Math.random()*152))}.png`;
     cardImage.appendChild(newImage)
-    }
+        
 
 })
+
+
+resetButton.addEventListener('click', function(){
+    
+//document.getElementById('pokeImage').src = '';
+document.getElementById('pokeImage').remove('');
+
+
+}) 
+
+
 
