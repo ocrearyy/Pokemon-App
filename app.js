8132975b1f1596156cb2a084c76d6b31d3c80f0f
oshanedesign <oshane.design@gmail.com>
2022-03-21 23:22:18 -0500

    updated dynamically using JS

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
 // Each pokemon has a number. Change the number in the source to find a new pokemon. 
 // This is the source for the pokemon images: https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
+const div = document.createElement('div');
+div.innerHTML = `<section class="section">
+<nav class="level">
+    <div class="level-item has-text-centered">
+      <div>
+<h1 class="title ">My Pokemon App</h1>
+</nav>
+<div class="columns">
+    <div class="column is-one-third">
+    </div>
+    <div  class="column is-one-third">
+        <div  class="card">
+            <div class="card-image">
+              <figure id="cardImage" class="image is-4by3">
+                <img  src="https://cdn.dribbble.com/users/458522/screenshots/2836818/media/2270b839abc833ef3002be3d1d4ec99d.jpg?compress=1&resize=800x600&vertical=top" alt="Placeholder image">
+                <img id='pokeImage'  alt="">
+              </figure>
+            </div>
+            <div class="card-content">
+                <nav class="level">
+                    <div class="level-item has-text-centered">
+                      <div>
+                <div class="media-content">
+                  <p class="title is-4">Choose Your Pokemon</p>
+                  <button id ="pokemonButton"class="button is-link is-rounded">Choose</button>
+                  <button id ="resetButton"class="button is-link is-rounded">Reset </button>
+
+                </div>
+              </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+    <div class="column is-one-third">
+    </div>
+    
+</div>  
+</section>`
+document.body.append(div);
 
 const pokemonButton = document.querySelector('#pokemonButton')
 const cardImage = document.querySelector('#cardImage')
@@ -15,6 +54,4 @@ pokemonButton.addEventListener ('click', function() {
 resetButton.addEventListener('click', function(){  
     document.getElementById('pokeImage').remove('');
 }) 
-
-
 
